09454aa8ef4a112e601c946cf2d321f5da8b1b78
Bhanu262626 <bhanuprakashreddy.sidhapureddy@cuw.edu>
2026-05-16 16:44:25 -0400

    Initial Spring AI enterprise setup

---
+++ src/main/java/com/example/eka/EnterpriseKnowledgeAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.eka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseKnowledgeAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseKnowledgeAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eka/EnterpriseKnowledgeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseKnowledgeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
